bc198b04561cb4377867b9352eef68ea3f812c21
Rosemary Marano <rosemary.marano@oracle.com>
2019-06-21 16:23:31 -0400

    update copyright info

--- config_coordinator/config_coordinator_test.go
+++ config_coordinator/config_coordinator_test.go
@@ -1,6 +1,6 @@
 package main
 /*
- * Copyright (c) 2017 Oracle and/or its affiliates
+ * Copyright (c) 2017, 2019, Oracle Corporation and/or its affiliates. All rights reserved.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
  */
