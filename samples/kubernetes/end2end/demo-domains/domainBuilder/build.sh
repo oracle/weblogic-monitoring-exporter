7264e8c69711900951768636115c4b3b2a137ac3
Lily He <lily.he@oracle.com>
2020-01-09 00:22:18 -0800

    add comments

--- samples/kubernetes/end2end/demo-domains/domainBuilder/build.sh
+++ samples/kubernetes/end2end/demo-domains/domainBuilder/build.sh
@@ -16,7 +16,7 @@ function createArchive() {
   mkdir -p ${TMP_DIR}/archive/wlsdeploy/applications
 
   echo 'Build the test webapp...'
-  # Run jar cmd in a docker container to avoid jar dependency to the host machine
+  # Run jar cmd in a docker container to avoid introducing jar cmd dependency to the host machine
   docker run --rm -it -u 0 -v ${PWD}/test-webapp:/tt store/oracle/weblogic:12.2.1.3 bash /tt/build.sh
   cp test-webapp/testwebapp.war ${TMP_DIR}/archive/wlsdeploy/applications/testwebapp.war
 
--- samples/kubernetes/end2end/demo-domains/domainBuilder/test-webapp/build.sh
+++ samples/kubernetes/end2end/demo-domains/domainBuilder/test-webapp/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This scripts is supposed to be run in a docker container to build the webapp war file.
 
 cd /tt
 jar cf testwebapp.war index.jsp WEB-INF/weblogic.xml WEB-INF/web.xml
