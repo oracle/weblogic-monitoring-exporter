0739ed9f5f7cd799c8c38ee0bf110ec9a9765903
Russell Gold <russell.gold@oracle.com>
2020-09-17 13:39:21 -0400

    Fix copyrights

--- wls-exporter-core/src/test/java/com/oracle/wls/exporter/WebClient8ImplTest.java
+++ wls-exporter-core/src/test/java/com/oracle/wls/exporter/WebClient8ImplTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2020, Oracle Corporation and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package com.oracle.wls.exporter;
 
 public class WebClient8ImplTest extends WebClientTestBase {
