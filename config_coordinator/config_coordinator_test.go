80df5dbccb067105d21d110ed8d2cfb12834a082
Derek Sharpe <derek.sharpe@oracle.com>
2020-12-11 10:54:37 -0600

    Removed the word corporation from the copyright statement

--- config_coordinator/config_coordinator_test.go
+++ config_coordinator/config_coordinator_test.go
@@ -1,6 +1,6 @@
 package main
 /*
- * Copyright (c) 2017, 2020, Oracle Corporation and/or its affiliates.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
  */
