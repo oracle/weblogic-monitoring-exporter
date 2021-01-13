f3ad618065284b96783cbf3b590ca3bf31dbdbfc
Tomas Langer <tomas.langer@oracle.com>
2021-01-13 17:13:43 +0100

    Metrics support using Helidon WebServer with unit test example.

---
+++ wls-exporter-sidecar/src/test/java/com/oracle/wls/exporter/MultipartTestUtils.java
@@ -0,0 +1,41 @@
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package com.oracle.wls.exporter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
+public class MultipartTestUtils {
+
+    private final static String BOUNDARY = "C3n5NKoslNBKj4wBHR8kCX6OtVYEqeFYNjorlBP";
+
+    public static String getContentType() {
+        return "multipart/form-data; boundary=" + BOUNDARY;
+    }
+
+    public static String createEncodedForm(String effect, String configuration) throws IOException {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.setBoundary(BOUNDARY);
+        builder.addTextBody("effect", effect);
+        builder.addBinaryBody("configuration",
+                              configuration.getBytes(),
+                              ContentType.create("text/plain", Charset.defaultCharset()),
+                              "newconfig.yml");
+        HttpEntity entity = builder.build();
+        return asString(entity);
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    static String asString(HttpEntity entity) throws IOException {
+        byte[] result = new byte[(int) entity.getContentLength()];
+        InputStream inputStream = entity.getContent();
+        inputStream.read(result);
+        return new String(result);
+    }
+}
