582c8abfbb57a5cba4b10582f1c2cff0efb19930
Ryan Eberhard <ryan.eberhard@oracle.com>
2025-06-27 15:04:08 -0400

    Trap signals to allow container to exit cleanly

--- start_exporter.sh
+++ start_exporter.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+cleanup() {
+  echo "Suppressing signal to allow container to exit cleanly."
+  exit 0
+}
+
+trap cleanup SIGTERM SIGKILL
+
+exitMessage() {
+  echo "Exiting container for WebLogic monitoring exporter."
+}
+
+trap exitMessage EXIT
 
 java $JAVA_OPTS -jar wls-exporter-sidecar.jar
