bba36c80360c67ca1f2a477f8a280438b4e83b6e
Ryan Eberhard <ryan.eberhard@oracle.com>
2025-07-01 09:01:31 -0400

    Improve exporter start script

--- start_exporter.sh
+++ start_exporter.sh
@@ -15,4 +15,22 @@ exitMessage() {
 
 trap exitMessage EXIT
 
-java $JAVA_OPTS -jar wls-exporter-sidecar.jar
+terminate_flag=0
+trap 'echo Terminating WME; terminate_flag=1' TERM INT
+
+java $JAVA_OPTS -jar wls-exporter-sidecar.jar &
+wme_pid=$!
+
+wait $wme_pid
+exit_code=$?
+
+# If the shell receives a SIGTERM, it will not be propagated to children but 'wait' will exit
+# Send SIGTERM to children (assuming SIGTERM triggers children shutdown) and wait for children to exit
+if [[ ${terminate_flag} == 1 ]]; then
+  kill $wme_pid
+  wait $wme_pid
+  exit_code=$?
+fi
+
+echo 'WME exit code: '$exit_code
+exit 0
